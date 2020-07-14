47c50de5451a52fc0e2153b3cae358e366b2a4bf
guicgs <etohlif@protonmail.com>
2020-07-14 19:01:42 -0300

    CC

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -2,6 +2,9 @@ const recipeService = require('../services/recipeService');
 
 const listRecipes = async (_req, res) => {
   const recipes = await recipeService.getAllRecipes();
+
+  if (!recipes) return res.status(500).json({ message: 'Error when connecting to database' });
+
   return res.status(200).json(recipes);
 };
 
@@ -10,35 +13,43 @@ const showRecipe = async (req, res) => {
 
   const recipe = await recipeService.showRecipe(id);
 
+  if (!recipe) return res.status(500).json({ message: 'Error when connecting to database' });
+
   if (recipe.error) return res.status(recipe.code).json({ message: recipe.message });
 
   return res.status(200).json(recipe);
 };
 
+function validateNewRecipe({ name, ingredients, preparation }) {
+  const validName = name && typeof name === 'string';
+  const validIngredients = ingredients && typeof ingredients === 'string';
+  const validPreparation = preparation && typeof preparation === 'string';
+  if (!validName || !validIngredients || !validPreparation) return false;
+  return true;
+}
+
 const newRecipe = async (req, res) => {
   const { name, ingredients, preparation } = req.body;
   const { _id: userId } = req.user;
 
-  const validName = name && typeof name === 'string';
-  const validIngredients = ingredients && typeof ingredients === 'string';
-  const validPreparation = preparation && typeof preparation === 'string';
+  const validEntries = validateNewRecipe({ name, ingredients, preparation });
 
-  if (!validName || !validIngredients || !validPreparation) {
+  if (!validEntries) {
     return res.status(400).json({ message: 'Invalid entries. Try again.' });
   }
 
   const recipe = await recipeService.registerNewRecipe({ name, ingredients, preparation, userId });
 
-  if (recipe.error) return res.status(recipe.code).json({ message: recipe.message });
-
   if (!recipe) return res.status(500).json({ message: 'Error when creating new recipe' });
+
+  if (recipe.error) return res.status(recipe.code).json({ message: recipe.message });
 
   return res.status(201).json({ recipe });
 };
 
-const editRecipe = async (req, res) => {};
+const editRecipe = async (req, res) => { };
 
-const deleteRecipe = async (req, res) => {};
+const deleteRecipe = async (req, res) => { };
 
 module.exports = {
   listRecipes,
