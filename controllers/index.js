a280ea40f6ea1bdc4e4773faf419ac4e49134954
Douglas Henrique <doughrqe@gmail.com>
2020-07-19 23:20:21 -0300

    remove infos

--- controllers/index.js
+++ controllers/index.js
@@ -1,30 +1,17 @@
 const getRecipes = require('./getRecipes');
-// const recipeController = require('./recipeController');
 const recipeDetails = require('./recipeDetails');
-// const getNewUser = require('./getNewUser');
 const postNewUser = require('./postNewUser');
 const login = require('./login');
-// const getNewRecipe = require('./getNewRecipe');
 const postNewRecipe = require('./postNewRecipe');
 const recipeEdit = require('./recipeEdit');
-// const postNewEdit = require('./postNewEdit');
 const recipeDelete = require('./recipeDelete');
-// const postNewDelete = require('./postNewDelete');
-// const getSearch = require('./getSearch');
 
 module.exports = {
-  // userController,
-  // recipeController,
   recipeDetails,
-  // getNewUser,
   login,
   postNewUser,
   getRecipes,
-  // getNewRecipe,
   postNewRecipe,
   recipeEdit,
-  // postNewEdit,
   recipeDelete,
-  // postNewDelete,
-  // getSearch,
 };
--- index.js
+++ index.js
@@ -26,26 +26,6 @@ app.route('/recipes/:id')
   .post(authMiddleware, recipeEdit)
   .delete(authMiddleware, recipeDelete);
 
-// app.get('/admin', middlewares.auth(), (req, res) => {
-//   return res.render('admin/home', { user: req.user });
-// });
-
-// app.get('/recipes/new', middlewares.auth(), controllers.getNewRecipe);
-// app.get('/recipes/search', controllers.getSearch);
-// app.post('/recipes/:id', middlewares.auth(), controllers.postNewEdit);
-// app.get('/recipes/:id/edit', middlewares.auth(), controllers.recipeEdit);
-// app.get('/recipes/:id/delete', middlewares.auth(), controllers.recipeDelete);
-// app.post('/recipes/:id/delete', middlewares.auth(), controllers.postNewDelete);
-// app.get('/recipes/:id', middlewares.auth(false), controllers.recipeDetails);
-
-// app.get('/signup', controllers.getNewUser);
-// app.post('/signup', controllers.postNewUser);
-// app.get('/', middlewares.auth(false), controllers.recipeController);
-
-// app.get('/login', controllers.userController.loginForm);
-// app.get('/logout', controllers.userController.logout);
-// app.post('/login', controllers.userController.login);
-
 app.use('*', (_req, _res, next) => (
   next({ code: 'not_found', message: 'This is a not good option!' })
 ));
