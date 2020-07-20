8dcc56d44307468c98be7e3733cfbf8708dfdece
Anderson Bolivar <bolivar.anderson@hotmail.com>
2020-07-20 16:55:18 -0300

    Recipes Controller; Recipes Service

---
+++ controllers/recipesController.js
@@ -0,0 +1,100 @@
+const recipesService = require('../services/recipesService');
+
+const Boom = require('@hapi/boom');
+
+function handleErrorCreate(error) {
+  const { type, details } = error;
+
+  if (type === 'invalid-data') {
+    throw Boom.badRequest('Dados inválidos', details);
+  }
+
+  if (type === 'exist-recipe') {
+    throw Boom.badRequest('Receita já cadastrada pelo usuário');
+  }
+}
+
+async function create(req, res) {
+  try {
+    const { error, recipe } = await recipesService.create(req.body);
+
+    handleErrorCreate(error);
+
+    res.status(201).json({ recipe });
+  } catch (err) {
+    throw err;
+  }
+}
+
+function handleErrorFind(error) {
+  const { type } = error;
+
+  if (type === 'recipe-not-found') {
+    throw Boom.badRequest('Receita não encontrada');
+  }
+}
+
+async function find(req, res) {
+  try {
+    const { error, recipe } = await recipesService.find(req.params.id);
+
+    handleErrorFind(error);
+
+    res.status(200).json({ recipe });
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function list(_req, res) {
+  try {
+    const { recipes } = await recipesService.list();
+
+    res.status(200).json({ recipes });
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function remove(req, res) {
+  try {
+    await recipesService.remove(req.params.id);
+
+    res.status(200).json({ message: 'Receita removida com sucesso!' });
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function update(req, res) {
+  try {
+    const { recipe } = await recipesService.update(req.params.id);
+
+    handleErrorFind(error);
+
+    res.status(200).json({ recipe });
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function upadateImage(req, res) {
+  try {
+    const { recipe } = await recipesService.upadateImage(req.params.id);
+
+    handleErrorFind(error);
+
+    res.status(200).json({ recipe });
+  } catch (err) {
+    throw err;
+  }
+}
+
+module.exports = {
+  create,
+  find,
+  list,
+  remove,
+  update,
+  upadateImage,
+};
---
+++ services/recipesService.js
@@ -0,0 +1,110 @@
+const { recipesSchema } = require('./joinSchemas');
+
+const recipesModel = require('../models/recipesModel');
+
+async function create(body) {
+  try {
+    const { error, value } = recipesSchema.validate(body, {
+      abortEarly: false,
+    });
+
+    if (error) {
+      return {
+        error: { type: 'invalid-data', details: error.details.map(({ message }) => message) },
+        recipe: null,
+      };
+    }
+
+    const recipe = await recipesModel.create(value);
+
+    return { error: { type: null, details: null }, recipe };
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function find(id) {
+  try {
+    const recipe = await recipesModel.find(id);
+
+    if (!recipe) {
+      return {
+        error: { type: 'recipe-not-found' },
+        recipe: null,
+      };
+    }
+
+    return { error: { type: null }, recipe };
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function list() {
+  try {
+    const recipes = await recipesModel.list(id);
+
+    return { recipes };
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function remove(id) {
+  try {
+    const recipe = await recipesModel.find(id);
+
+    if (recipe) {
+      await recipesModel.delte(id);
+    }
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function update(id) {
+  try {
+    const recipe = await recipesModel.find(id);
+
+    if (!recipe) {
+      return {
+        error: { type: 'recipe-not-found' },
+        recipe: null,
+      };
+    }
+
+    const newRecipe = await recipesModel.update(id);
+
+    return { error: { type: null }, recipe: newRecipe };
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function upadateImage(id) {
+  try {
+    const recipe = await recipesModel.find(id);
+
+    if (!recipe) {
+      return {
+        error: { type: 'recipe-not-found' },
+        recipe: null,
+      };
+    }
+
+    const newRecipe = await recipesModel.upadateImage(id);
+
+    return { error: { type: null }, recipe: newRecipe };
+  } catch (err) {
+    throw err;
+  }
+}
+
+module.exports = {
+  create,
+  find,
+  list,
+  remove,
+  update,
+  upadateImage,
+};
