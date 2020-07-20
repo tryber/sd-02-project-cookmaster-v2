145b4241c6cf70af1b0c4151fbdfdcdf8f442d9c
Anderson Bolivar <bolivar.anderson@hotmail.com>
2020-07-20 14:19:27 -0300

    Middleware - auth

---
+++ middlewares/auth.js
@@ -0,0 +1,31 @@
+const Boom = require('@hapi/boom');
+
+const jwt = require('jsonwebtoken');
+
+const userModel = require('../models/userModel');
+
+async function auth(req, _res, next) {
+  try {
+    const token = req.headers['authorization'];
+
+    if (!token) {
+      throw Boom.badRequest('Token não encontrado ou informado');
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await userModel.find({ email: decoded.data.email });
+
+    if (!user) {
+      throw Boom.unauthorized('Erro ao procurar usuario referente ao token.');
+    }
+
+    req.user = user;
+
+    next();
+  } catch (err) {
+    next(err);
+  }
+}
+
+module.exports = auth;
