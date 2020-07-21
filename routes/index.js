abfe8a7d27848dcafca28db38e536d4ab7711c61
Anderson Bolivar <bolivar.anderson@hotmail.com>
2020-07-21 18:05:35 -0300

    Remove Imgae Router

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,8 @@
-// const imagesRouter = require('./imagesRouter');
 const loginRouter = require('./loginRouter');
 const recipesRouter = require('./recipesRouter');
 const userRouter = require('./userRouter');
 
 module.exports = {
-  // imagesRouter,
   loginRouter,
   recipesRouter,
   userRouter,
