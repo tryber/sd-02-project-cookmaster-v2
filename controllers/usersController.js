105b8dc20d6e3d407541e9d57a316a75aa7129db
richardwellerson <richardwellerson@gmail.com>
2020-08-31 13:58:27 -0300

    Cadastramento de admin feito

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,27 +1,69 @@
 const express = require('express');
-const userModel = require('../models/genericModel');
+const models = require('../models/genericModel');
+const middlewares = require('../middlewares');
+const JWT = require('jsonwebtoken');
 
 const usersRouter = express.Router();
 
-usersRouter.post('/', async (req, res) => {
-  const { name, email, password } = req.body;
-  const lastUser = await userModel.getLastData('users');
-  const { _id } = lastUser[0];
-  const newId = _id + 1 || 1;
+usersRouter
+  .post('/', async (req, res) => {
+    const { name, email, password } = req.body;
+    const lastUser = await models.getLastData('users');
+    const { _id } = lastUser[0];
+    const newId = _id + 1 || 1;
 
-  const { code } = await userModel.insert('users', { _id: newId, name, email, password, role: 'user' });
+    const { code } = await models.insert('users', { _id: newId, name, email, password, role: 'user' });
 
-  if (code) {
-    return res.status(409).json({ message: 'email exists in database', code: 'conflict' });
-  }
+    if (code) {
+      return res.status(409).json({ message: 'email exists in database', code: 'conflict' });
+    }
 
-  const newUser = await userModel.getLastData('users');
+    const newUser = await models.getLastData('users');
 
-  return res.status(201).json({
-    message: 'Success',
-    user: newUser[0],
-    token,
+    return res.status(201).json({
+      message: 'Success',
+      user: newUser[0],
+      token,
+    });
   });
-});
+
+usersRouter.post(
+  '/admin',
+  middlewares.authUser,
+  middlewares.isUserValid,
+  async (req, res) => {
+    const { role } = JWT.decode(req.headers.authorization);
+
+    if (role !== 'admin') {
+      return res.status(403).json({
+        message: 'You do not have rights to insert admin on database.',
+        code: 'unauthorized',
+      });
+    }
+
+    const { email } = req.body;
+
+    const existsUser = models.getBy('users', 'email', email);
+
+    if (existsUser.length) {
+      return res.status(409).json({
+        message: 'user exists in db',
+        code: 'conflict',
+      });
+    }
+
+    const lastUser = await models.getLastData('users');
+    const { _id } = lastUser[0];
+    const newId = _id + 1 || 1;
+
+    await models.insert('users', { _id: newId, ...req.body, role: 'admin' });
+
+    const { password, ...noPassword } = await models.getLastData('users');
+
+    return res.status(201).json({
+      status: 'success',
+      user: noPassword,
+    })
+  });
 
 module.exports = usersRouter;
